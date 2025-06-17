fcb9aba3cc0f5fbf02342eae566d6bf52aad7f86
Rapeti Veda Priya <vedapr471@gmail.com>
2025-06-17 11:18:00 +0000

    update HireSmart/ui/app.py

--- HireSmart/ui/app.py
+++ HireSmart/ui/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from Agents.interview_agent import MockInterviewAgent
+from HireSmart/HireSmart/Agents.interview_agent import MockInterviewAgent
 
 agent = MockInterviewAgent()
 
